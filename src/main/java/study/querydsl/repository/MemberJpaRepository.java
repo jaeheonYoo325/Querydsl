aef9370a1a4b6fc3037fc0d705630eccba4c1980
jaeheonYoo325 <yoojh0325@gmail.com>
2021-09-04 01:23:39 +0900

    2021.09.04 : 동적 쿼리와 성능 죄적화 조회 - Where절 파라미터 사용.

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import study.querydsl.dto.MemberSearchCondition;
@@ -110,8 +112,45 @@ public class MemberJpaRepository {
 								team.name.as("teamName")
 						))
 						.from(member)
-						.where(builder)
 						.leftJoin(member.team, team)
+						.where(builder)
 						.fetch();
 	}
+	
+	public List<MemberTeamDto> search(MemberSearchCondition condition) {
+		return queryFactory
+						.select(new QMemberTeamDto(
+								member.id.as("memberId"),
+								member.username,
+								member.age,
+								team.id.as("teamId"),
+								team.name.as("teamName")
+						))
+						.from(member)
+						.leftJoin(member.team, team)
+						.where(
+								usernameEq(condition.getUsername()),
+								teamNameEq(condition.getTeamName()),
+								ageGoe(condition.getAgeGoe()),
+								ageLoe(condition.getAgeLoe())
+						)
+						.fetch();
+	}
+	
+	private BooleanExpression usernameEq(String username) {
+		return StringUtils.hasText(username) ? member.username.eq(username) : null;
+	}
+
+	private BooleanExpression teamNameEq(String teamName) {
+		return StringUtils.hasText(teamName) ? team.name.eq(teamName) : null;
+	}
+	
+	private BooleanExpression ageGoe(Integer ageGoe) {
+		return ageGoe != null ? member.age.goe(ageGoe) : null;
+	}
+	
+	private BooleanExpression ageLoe(Integer ageLoe) {
+		return ageLoe != null ? member.age.loe(ageLoe) : null;
+	}
+	
 }
