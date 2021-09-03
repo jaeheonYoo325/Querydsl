b0d26a979af40ad44188343023c5e13a33df0dfb
jaeheonYoo325 <yoojh0325@gmail.com>
2021-09-03 20:04:48 +0900

    2021.09.03 : 수정, 삭제 벌크 연산.

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -38,6 +38,8 @@ import study.querydsl.entity.Team;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+
 import static com.querydsl.jpa.JPAExpressions.select;
 
 @SpringBootTest
@@ -788,5 +790,63 @@ class QuerydslBasicTest {
 
 	private BooleanExpression allEq(String usernameCond, Integer ageCond) {
 		return usernameEq(usernameCond).and(ageEq(ageCond));
+	}
+	
+	@Test
+	public void bulkUpdate() {
+		
+		//bulk연산 전
+		//member1 = 10 -> DB member1
+		//member2 = 20 -> DB member2
+		//member3 = 30 -> DB member3
+		//member4 = 40 -> DB member4
+		                   
+		long count = queryFactory
+				.update(member)
+				.set(member.username, "비회원")
+				.where(member.age.lt(28))
+				.execute();
+		
+		//bulk연산 후
+		//member1 = 10 -> DB 비회원
+		//member2 = 20 -> DB 비회원
+		//member3 = 30 -> DB member3
+		//member4 = 40 -> DB member4
+		
+		em.flush();
+		em.clear();
+		
+		List<Member> result = queryFactory
+				.selectFrom(member)
+				.fetch();
+		
+		for( Member member1 : result ) {
+			System.out.println("member1 = " + member1);
+		}
+	}
+	
+	@Test
+	public void bulkAdd() {
+		long count = queryFactory
+				.update(member)
+				.set(member.age, member.age.add(1))
+				.execute();
+				
+	}
+	
+	@Test
+	public void bulkMultiply() {
+		long count = queryFactory
+				.update(member)
+				.set(member.age, member.age.multiply(2))
+				.execute();
+	}
+	
+	@Test
+	public void bulkDelete() {
+		long count = queryFactory
+				.delete(member)
+				.where(member.age.gt(18))
+				.execute();
 	}
 }
